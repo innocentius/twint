f415cc70380a5731805b39a1f39edda381cc7481
Innocentius <innocentius@users.noreply.github.com>
2020-12-18 20:24:40 +0800

    Update db.py

--- twint/storage/db.py
+++ twint/storage/db.py
@@ -310,7 +310,7 @@ def tweets(conn, Tweet, config):
         if Tweet.reply_to:
             for reply in Tweet.reply_to:
                 query = 'INSERT INTO replies VALUES(?,?,?)'
-                print(reply, flush = true)
+                print(reply, flush = True)
                 cursor.execute(query, (Tweet.id, int(reply['user_id']), reply['username']))
 
         conn.commit()
